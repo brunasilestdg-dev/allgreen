1b612426eb771f3d2159e1b91317bc17d467f621
brunapsiles <brunapsiles@gmail.com>
2026-09-17 18:02:56 -0300

    fix(allgreen): carregar acabamento final da sidebar

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import "./features/logistics/LogisticsVerticalAccess.css";
 import "./features/logistics/LogisticsVerticalEnterprise.css";
 import "./features/logistics/LogisticsVerticalRecovery.css";
 import "./features/logistics/LogisticsVerticalEnterpriseRefinement.css";
+import "./features/logistics/LogisticsVerticalEnterpriseShellPolish.css";
 import "./features/logistics/LogisticsVerticalRecovery.js";
 import "./features/logistics/LogisticsVerticalCredentials.js";
 // WorkCenterV2 é lazy-loaded quando acessado (ARQ-01 otimização)
