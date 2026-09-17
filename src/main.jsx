209f7823151a96b5333002c927ea1f2a2cd04279
brunapsiles <brunapsiles@gmail.com>
2026-09-17 18:31:59 -0300

    ui(allgreen): ativar visual system v2

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,7 @@ import "./features/logistics/LogisticsVerticalEnterpriseRefinement.css";
 import "./features/logistics/LogisticsVerticalEnterpriseShellPolish.css";
 import "./features/logistics/ErpHomeEnterprise.css";
 import "./features/logistics/LogisticsVerticalMenuCompact.css";
+import "./features/logistics/AllGreenVisualSystemV2.css";
 import "./features/logistics/allGreenMenuEnhancements.js";
 import "./features/logistics/LogisticsVerticalRecovery.js";
 import "./features/logistics/LogisticsVerticalCredentials.js";
